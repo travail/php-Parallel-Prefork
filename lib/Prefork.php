28c4c4e23cd70beb7e9fb0e07490ef194ff609e9
travail <sawa.tomoyuki@gmail.com>
2014-04-30 12:41:51 +0900

    CamelCase to snake_case refs #430: Tidy up the code

--- lib/Prefork.php
+++ lib/Prefork.php
@@ -6,90 +6,86 @@ declare(ticks = 1);
 
 class Prefork
 {
-    public $trapSignals = array(
-        SIGTERM => SIGTERM,
-    );
-    public $maxWorkers         = 3;
-    public $errRespawnInterval = 1;
-    public $managerPid         = null;
-    public $onChildReap        = null;
-    public $signalReceived     = null;
-    private $inChild           = false;
-    private $workerPids        = array();
-    private $generation        = 0;
+    public $trap_signals         = array(SIGTERM => SIGTERM);
+    public $max_workers          = 10;
+    public $err_respawn_interval = 1;
+    public $manager_pid          = null;
+    public $on_child_reap        = null;
+    public $signal_received      = null;
+    private $in_child            = false;
+    private $worker_pids         = array();
+    private $generation          = 0;
 
     function __construct($args = array())
     {
-        $this->maxWorkers = isset($args['max_workers'])
-            ? $args['max_workers'] : $this->maxWorkers;
-        $this->trapSignals = isset($args['trap_signals'])
-            ? $args['trap_signals'] : $this->trapSignals;
+        $this->max_workers = isset($args['max_workers'])
+            ? $args['max_workers'] : $this->max_workers;
+        $this->trap_signals = isset($args['trap_signals'])
+            ? $args['trap_signals'] : $this->trap_signals;
 
-        foreach (array_keys($this->trapSignals) as $sig) {
+        foreach (array_keys($this->trap_signals) as $sig) {
             pcntl_signal($sig, array($this, '_signalHandler'), false);
         }
     }
 
     private function _signalHandler(&$sig)
     {
-        $this->signalReceived = $sig;
+        $this->signal_received = $sig;
     }
 
     public function start()
     {
-        $this->managerPid     = posix_getpid();
-        $this->signalReceived = null;
+        $this->manager_pid     = posix_getpid();
+        $this->signal_received = null;
         $this->generation++;
 
-        if ($this->inChild) {
+        if ($this->in_child) {
             die("Cannot start another process while you are in child process\n");
         }
 
         // for debugging
-        if ($this->maxWorkers === 0) {
-            return true;
-        }
+        if ($this->max_workers === 0) return true;
 
         // main loop
-        while ($this->signalReceived === null) {
+        while ($this->signal_received === null) {
             $pid = null;
-            if (count(array_keys($this->workerPids)) < $this->maxWorkers) {
+            if (count(array_keys($this->worker_pids)) < $this->max_workers) {
                 $pid = pcntl_fork();
                 if ($pid === -1) {
                     echo "fork failed!\n";
-                    sleep($this->errRespawnInterval);
+                    sleep($this->err_respawn_interval);
                     continue;
                 }
                 if ($pid === 0) {
                     // child process
-                    $this->inChild = true;
-                    foreach (array_keys($this->trapSignals) as $sig) {
+                    $this->in_child = true;
+                    foreach (array_keys($this->trap_signals) as $sig) {
                         pcntl_signal($sig, SIG_DFL, true);
                     }
-                    if ($this->signalReceived !== null) {
+                    if ($this->signal_received !== null) {
                         exit(0);
                     }
                     return false;
                 }
-                $this->workerPids[$pid] = $this->generation;
+                $this->worker_pids[$pid] = $this->generation;
             }
-            $exitPid = is_null($pid)
+            $exit_pid = is_null($pid)
                 ? pcntl_waitpid(-1, $status)
                 : pcntl_waitpid(-1, $status, WNOHANG);
-            if ($exitPid > 0 && isset($status)) {
-                $this->_runChildReapCb($exitPid, $status);
+            if ($exit_pid > 0 && isset($status)) {
+                $this->_runChildReapCb($exit_pid, $status);
                 if (
-                    isset($this->workerPids[$exitPid])
-                    && $this->workerPids[$exitPid] === $this->generation
+                    isset($this->worker_pids[$exit_pid])
+                    && $this->worker_pids[$exit_pid] === $this->generation
                     && pcntl_wifexited($status) !== true
                     ) {
-                    sleep($this->errRespawnInterval);
+                    sleep($this->err_respawn_interval);
                 }
-                unset($this->workerPids[$exitPid]);
+                unset($this->worker_pids[$exit_pid]);
             }
         }
         // send signals to workers
-        if ($sig = $this->trapSignals[$this->signalReceived]) {
+        if ($sig = $this->trap_signals[$this->signal_received]) {
             $this->signalAllChildren($sig);
         }
 
@@ -98,17 +94,16 @@ class Prefork
 
     public function finish($exitCode = 0)
     {
-        if ($this->maxWorkers === 0) {
-            return;
-        }
+        if ($this->max_workers === 0) return;
+
         exit($exitCode);
     }
 
     public function waitAllChildren()
     {
-        foreach (array_keys($this->workerPids) as $pid) {
-            if ($exitPid = pcntl_wait($status)) {
-                unset($this->workerPids[$exitPid]);
+        foreach (array_keys($this->worker_pids) as $pid) {
+            if ($exit_pid = pcntl_wait($status)) {
+                unset($this->worker_pids[$exit_pid]);
                 $this->_runChildReapCb($pid, $status);
             }
         }
@@ -116,14 +111,14 @@ class Prefork
 
     public function signalAllChildren($sig)
     {
-        foreach (array_keys($this->workerPids) as $pid) {
+        foreach (array_keys($this->worker_pids) as $pid) {
             posix_kill($pid, $sig);
         }
     }
 
     public function signalReceived()
     {
-        return $this->signalReceived;
+        return $this->signal_received;
     }
 
     public function generation()
@@ -131,17 +126,16 @@ class Prefork
         return $this->generation;
     }
 
-    // PHP 5.2.x ain't implemented lamda function
-    private function _runChildReapCb($exitPid, $status)
+    // PHP 5.2.x don't have lamda function
+    private function _runChildReapCb($exit_pid, $status)
     {
-        $cb = $this->onChildReap;
+        $cb = $this->on_child_reap;
         if ($cb) {
 /*
             try {
-                $cb->($exitPid, $status);
+                $cb->($exit_pid, $status);
             }
 */
-            // XXX - hmph, what to do hear?
         }
     }
 }
